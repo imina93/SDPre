a666b922a0fc9ae4cc7aac49af2370fb3c8e1641
Isaac Mina <isaacmina93@gmail.com>
2021-07-07 11:32:49 -0700

    complete objective4lab2

--- Labs/Objective4Lab1.java
+++ Labs/Objective4Lab1.java
@@ -6,24 +6,18 @@ public class Objective4Lab1 {
     Scanner keyboard = new Scanner(System.in);
     System.out.println("What is your first name?");
     String fname = keyboard.nextLine();
-    System.out.println(fname);
 
     System.out.println("What is your last name?");
     String lname = keyboard.nextLine();
-    System.out.println(lname);
 
     System.out.println("What is your favorite animal?");
     String favoriteAnimal = keyboard.nextLine();
-    System.out.println(favoriteAnimal);
 
     System.out.println("What is your favorite food?");
     String favoriteFood = keyboard.nextLine();
-    System.out.println(favoriteFood);
 
     System.out.println("What is your favorite song?");
     String favoriteSong = keyboard.nextLine();
-    System.out.println(favoriteSong);
-
 
     System.out.println("My favorite animal is " + favoriteAnimal);
     System.out.println("My favorite food is " + favoriteFood);
