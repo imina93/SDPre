3e3a5c828ce98325e82864a285a2aa80a759d8e2
Isaac Mina <isaacmina93@gmail.com>
2021-07-06 17:01:20 -0700

    complete objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,11 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Isaac Mina";
+    int age = 27;
+    String state = "California";
+      System.out.println(name);
+      System.out.println(age);
+      System.out.println(state);
+
+  }
+}
