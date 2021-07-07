6fe1517e76f8afb09964a399fca27ee194941624
Isaac Mina <isaacmina93@gmail.com>
2021-07-06 17:12:17 -0700

    complete objective2lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,11 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+
+    num1 = 12;
+    num2 = 8;
+    num3 = 23;
+    average = 14.33333333333;
+    System.out.println(average);
+  }
+}
