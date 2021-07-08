c7b34387775f57272d3696cd971cb6736a9d9b1a
Isaac Mina <isaacmina93@gmail.com>
2021-07-08 00:09:34 -0700

    complete objective8lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,15 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+    int num;
+    //TODO Write a for loop that will print the numbers 1-10
+    for (num = 0; num < 11; num+=1) {
+      System.out.println(num);
+    }
+    for (num = 10; num > 0; num-=1) {
+      System.out.println(num);
+    }
+    //TODO Write another for loop that will print the numbers 10-1.
+
+
+  }
+}
