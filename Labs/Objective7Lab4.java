035e8dd618b3afc4d6b67c0569a10def7e0b0435
Isaac Mina <isaacmina93@gmail.com>
2021-07-07 22:22:42 -0700

    complete objective7lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,11 @@
+public class Objective7Lab4 {
+  public static void main(String[] args) {
+    int count = 0, sum = 0;
+    while(count <= 20) {
+      sum = sum + count;
+      count = count + 1;
+
+    }
+    System.out.println(sum);
+  }
+}
